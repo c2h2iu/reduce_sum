8ff7007cc51fbe3a43ea11c8e6b83c7142ed0d87
c2h2iu <chenghsiang.chiu@gmail.com>
2020-10-01 21:07:26 -0600

    change operation to bop

--- reduce_api.hpp
+++ reduce_api.hpp
@@ -8,19 +8,19 @@
 #include <iterator>
 
 
-namespace chiu{	
+namespace chiu{
     int numThreads = 256;
     unsigned int numBlocks = 0;
     
 
     template<typename T, typename C>
     __device__ void warpReduce(volatile T* sdata, const T& tid, const T& length, C&& bop){
-        if((length > 32) && (tid + 32 < length))    sdata[tid] += sdata[tid + 32];
-	if((length > 16) && (tid + 16 < length))    sdata[tid] += sdata[tid + 16];
-	if((length > 8)  && (tid + 8  < length))    sdata[tid] += sdata[tid + 8];
-	if((length > 4)  && (tid + 4  < length))    sdata[tid] += sdata[tid + 4];
-	if((length > 2)  && (tid + 2  < length))    sdata[tid] += sdata[tid + 2];
-	if((length > 1)  && (tid + 1  < length))    sdata[tid] += sdata[tid + 1];
+        if((length > 32) && (tid + 32 < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 32]);
+	      if((length > 16) && (tid + 16 < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 16]);
+	      if((length > 8)  && (tid + 8  < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 8]);
+	      if((length > 4)  && (tid + 4  < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 4]);
+	      if((length > 2)  && (tid + 2  < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 2]);
+	      if((length > 1)  && (tid + 1  < length))    sdata[tid] = bop(sdata[tid], sdata[tid + 1]);
     }
 
 
@@ -28,30 +28,29 @@ namespace chiu{
     __global__ void kernel(T* d_in, T* d_out, const unsigned int length, C&& bop){
         extern __shared__ T sdata[];
 	
-	T tid = threadIdx.x;
-	T i   = threadIdx.x + blockIdx.x * (blockDim.x * 2);
+	      T tid = threadIdx.x;
+        T i   = threadIdx.x + blockIdx.x * (blockDim.x * 2);
 
-	sdata[tid] = 0;
+	      sdata[tid] = 0;
        
-	if(i < length){
+	      if(i < length){
             if(gridDim.x > 1){
-                if(i + blockDim.x < length)    sdata[tid] = d_in[i] + d_in[i + blockDim.x];
-		else                           sdata[tid] = d_in[i];
-	    }
-	    else    sdata[tid] = d_in[i];
-	}
-	__syncthreads();
+                if(i + blockDim.x < length)    sdata[tid] = bop(d_in[i], d_in[i + blockDim.x]);
+		            else                           sdata[tid] = d_in[i];
+	          }
+	          else    sdata[tid] = d_in[i];
+	      }
+	      __syncthreads();
 
 
-	for(T s = blockDim.x/2; s > 32; s>>= 1){
-            if(tid < s)    sdata[tid] += sdata[tid + s];
-	    __syncthreads();
-	}
+	     for(T s = blockDim.x/2; s > 32; s>>= 1){
+           if(tid < s)    sdata[tid] = bop(sdata[tid], sdata[tid + s]);
+	         __syncthreads();
+	     }
 
+	     if(tid < 32)    warpReduce(sdata, tid, length, bop);
 
-	if(tid < 32)    warpReduce(sdata, tid, length, bop);
-
-	if(tid == 0)    d_out[blockIdx.x] = sdata[0];
+	     if(tid == 0)    d_out[blockIdx.x] = sdata[0];
     }
 
     
@@ -59,78 +58,51 @@ namespace chiu{
     T reduce(I beg, I end, T& init, C&& bop){
         unsigned int length = std::distance(beg, end); 
 	
-	if(length == 1)    return bop(init, *beg);
+	      if(length == 1)    return bop(init, *beg);
 
         using U = typename std::decay<decltype(*beg)>::type; 
 	
         U size = length * sizeof(U);
         U out_size = ((length-1)/numThreads+1) * sizeof(U);	
 
-	std::vector<U> h_out(out_size, 0);
+	      std::vector<U> h_out(out_size, 0);
 	
-	U* d_in;
-	U* d_out;
+	      U* d_in;
+	      U* d_out;
 
-	cudaMalloc(&d_in,  size);
-	cudaMalloc(&d_out, out_size);
+	      cudaMalloc(&d_in,  size);
+	      cudaMalloc(&d_out, out_size);
 
-	cudaMemcpy(d_in, &(*beg), size, cudaMemcpyHostToDevice);
+	      cudaMemcpy(d_in, &(*beg), size, cudaMemcpyHostToDevice);
          
         while(length > 1){
             if(length < numThreads)    numBlocks = 1;
-	    else                       numBlocks = (length - 1) / numThreads + 1;
+	          else                       numBlocks = (length - 1) / numThreads + 1;
 	
-	    kernel<<<numBlocks, numThreads, numThreads * sizeof(U)>>>(d_in, d_out, length, bop);
+	          kernel<<<numBlocks, numThreads, numThreads * sizeof(U)>>>(d_in, d_out, length, bop);
 	    
-	    length = (numBlocks - 1) / 2 + 1;
+	          length = (numBlocks - 1) / 2 + 1;
 
-	    d_in = d_out;
-	}
+	          d_in = d_out;
+	      }
 
-	cudaMemcpy(h_out.data(), d_out, out_size, cudaMemcpyDeviceToHost);
+	      cudaMemcpy(h_out.data(), d_out, out_size, cudaMemcpyDeviceToHost);
 
-	cudaFree(d_in);
-	cudaFree(d_out);
+	      cudaFree(d_in);
+	      cudaFree(d_out);
 
         return bop(init, h_out[0]);
     }
-
-
-    template<typename T>
-    struct plus{
-	__host__ __device__
-        constexpr T operator()(const T &lhs, const T &rhs) const{
-            return lhs + rhs;
-        }        
-    };
-
-
-    template<typename T>
-    struct maximum{
-        __host__ __device__
-	constexpr T operator()(const T &lhs, const T &rhs) const{
-            return (lhs > rhs) ? lhs : rhs;
-	}
-    };
-
-
-    template<typename T>
-    struct minimum{
-        __host__ __device__
-	constexpr T operator()(const T &lhs, const T &rhs) const{
-            return (lhs < rhs) ? lhs : rhs;
-	}
-    };
 }
 
 
 
-template<typename T, typename I>
-T verify(const std::vector<T>& array, I& init){
+template<typename T, typename I, typename C>
+T verify(const std::vector<T>& array, I& init, C&& bop){
     T res = 0;
-    std::for_each(array.begin(), array.end(), [&](T n){ res+= n; });
+    for(auto a : array)    res = bop(res, a);
  
-    return res+init;
+    return bop(res, init);
 }
 
 
@@ -144,22 +116,3 @@ void gen_array(std::vector<T>& array){
     });
 }
 
-/***
-int main(int argc, char* argv[]){	
-    unsigned int length = std::stoi(argv[1]);
-    
-    int init = 100;
-
-    std::vector<unsigned int> array(length, 0);
-
-    gen_array(array);
-
-    unsigned int my_sum = chiu::reduce(array.begin(), array.end(), init, chiu::plus<unsigned int>());
-
-    assert(my_sum == verify(array,init));
-
-    std::cout << "array of " << length << " elements passed.\n";
-   
-    return 0;
-} 
-***/
